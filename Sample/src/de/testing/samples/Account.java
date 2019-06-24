7adf0fc674e212e1e96e5ba1e589a8f8a2919d20
jpma89 <paddy_klein@web.de>
2019-06-24 15:12:27 +0200

    Test

---
+++ Sample/src/de/testing/samples/Account.java
@@ -0,0 +1,5 @@
+package de.testing.samples;
+
+public class Account {
+
+}
---
+++ Sample/src/de/testing/samples/AccountTes.java
@@ -0,0 +1,35 @@
+package de.testing.samples;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AccountTes {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	@DisplayName("second account should have account number = 2")
+	void testAccount() {
+		fail("Not yet implemented");
+	}
+}
